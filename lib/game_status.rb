1f20efeea66e6be8345ea879739f32c37dda78ca
Robert Watson <spartan83@gmail.com>
2017-12-02 22:21:33 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,11 +6,11 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
    [0,1,2], # Top row
-   [3,4,5]  # Middle row
-   [6,7,8]  # Bottom row
-   [0,3,6]  # Left col
-   [1,4,7]  # Middle col
-   [2,5,8]  #Right col
-   [0,4,8] #Diag Right
+   [3,4,5],  # Middle row
+   [6,7,8],  # Bottom row
+   [0,3,6],  # Left col
+   [1,4,7],  # Middle col
+   [2,5,8],  #Right col
+   [0,4,8], #Diag Right
    [2,4,6] #diag left
  ]
